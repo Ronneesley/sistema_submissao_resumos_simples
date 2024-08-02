ee4036a3df167e330cfdabcdbc971b506dbcd1c1
Ronneesley <ronneesley@gmail.com>
2024-08-01 21:47:22 -0300

    Exemplo de preenchimento

---
+++ documentacao/bd/02_preenchimento.sql
@@ -0,0 +1,5 @@
+insert into cursos(nome) values('Análise de Sistemas');
+insert into cursos(nome) values('Agronomia');
+insert into cursos(nome) values('Biologia');
+insert into cursos(nome) values('Computação');
+insert into cursos(nome) values('Artes');
