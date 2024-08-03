4d03c5cf5508892ecdd58c13991720246b0e2481
rodrigues396 <thallisonsiqueira@gmail.com>
2024-08-03 20:51:59 -0300

    Cadastrando(inserir.php) autor

---
+++ src/cadastro_autor/inserir.php
@@ -0,0 +1,17 @@
+<?php
+
+$nome = $_POST["nome"];
+$cpf = $_POST["cpf"];
+$email = $_POST["email"];
+$curso = $_POST["curso"];
+$instituicao_ensino = $_POST["instituicao_ensino"];
+$senha = $_POST["senha"];
+
+$con = new mysqli("localhost", "root", "", "evento");
+$con->query("insert into autores(nome, cpf, email, curso, instituicao_ensino, senha) values('$nome', '$cpf', '$email', '$curso', '$instituicao_ensino', '$senha')");
+
+$con->close();
+
+echo "Autor salvo  cadastrado com sucesso!";
+
+?>
