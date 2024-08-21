beebaf98d3082581d3f582f4a3d4976a7df32ae3
carlos eduardo <carlos.lopes4@estudante.ifto.edu.br>
2024-08-21 20:23:17 -0300

    d

--- src/organizador/cadastro_evento/evento.php
+++ src/organizador/cadastro_evento/evento.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro de Evento</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="evento.css">
 </head>
 
 <body>
