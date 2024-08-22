9905bd1dc88b4a7bbfe60c4bca725bdb7157488f
carlos eduardo <carlos.lopes4@estudante.ifto.edu.br>
2024-08-22 20:03:09 -0300

    88

--- src/organizador/cadastro_autor/cadastro.php
+++ src/organizador/cadastro_autor/cadastro.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro de Autor</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../../css/cadastro_evento.css">
 </head>
 <body>
     <header>
