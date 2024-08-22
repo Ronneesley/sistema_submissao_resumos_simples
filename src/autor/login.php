91893628932a42fe31577946174bca74c26c3380
carlos eduardo <carlos.lopes4@estudante.ifto.edu.br>
2024-08-22 20:36:34 -0300

    feito

--- src/autor/login.php
+++ src/autor/login.php
@@ -1,18 +1,20 @@
 <!DOCTYPE html>
 <html>
+<head>
+    <title> Login de autor</title>
 
+    <link rel="stylesheet" href="../css/login.css">
+</head>
 <body>
     <div class="login-form">
         <img src="SSR.png" alt="Logo">
-        <link rel="stylesheet" href="../../css/cadastro_evento.css">
-        <h2>Pagina do Autor</h2><br>
+        <h2>Login do Autor</h2><br>
         <input type="text" id="email" placeholder="Email" required>
         <input type="password" id="senha" placeholder="Senha" required>
         <p><a href="#">Esqueci minha senha</a></p>
         <button type="submit" onClick='entrar()'>ENTRAR</button>
         <button type="submit" onClick='cadastrar()'>CADASTRAR</button>
     </div>
-
     <script src="./index.js"></script>
 </body>
 </html>
