4363ad05c73cd6a9b0d456da23046046b84608bf
unknown <Aluno@CLN-041445.ifto.local>
2024-08-21 21:38:16 -0300

    f

--- src/organizador/cadastro_autor/listagem.php
+++ src/organizador/cadastro_autor/listagem.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Listagem de autores</title>
-    <link rel="stylesheet" href="listagem.css">
+    <link rel="stylesheet" href="listagem_autor.css">
 </head>
 <body>
 <?php
