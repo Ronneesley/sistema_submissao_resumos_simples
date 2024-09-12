d2132dcdfd3e9d3c69ebf0a63299dca4d86af254
unknown <Aluno@CLN-041445.ifto.local>
2024-09-11 22:20:30 -0300

    #79

--- src/organizador/cadastro_avaliador/listagem.php
+++ src/organizador/cadastro_avaliador/listagem.php
@@ -4,6 +4,7 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
+    
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SSRS - Sistema de Submissão de Resumos Simples</title>
