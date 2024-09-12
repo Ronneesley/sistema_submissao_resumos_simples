12431b91ded154ba0415d484adc2b66e106437e0
carlos <carlos.lopes4@estudante.ifto.edu.br>
2024-09-12 18:21:41 -0300

    .

--- src/organizador/cadastro_curso/listagem.php
+++ src/organizador/cadastro_curso/listagem.php
@@ -1,45 +1,31 @@
 <?php include("../layout_superior.php") ?>
 <?php include_once '../verifica_sessao.php'; ?>
+
 <?php
-
 $con = new mysqli("localhost", "root", "", "evento");
-$resultado = $con->query("select * from cursos order by nome");
 
+
+$filtro = isset($_GET['filtro']) ? $_GET['filtro'] : '';
+
+$sql = "SELECT * FROM cursos WHERE nome LIKE ? ORDER BY nome";
+$stmt = $con->prepare($sql);
+$likeFiltro = '%' . $filtro . '%';
+$stmt->bind_param('s', $likeFiltro);
+$stmt->execute();
+$resultado = $stmt->get_result();
 ?>
 <link rel="stylesheet" href="../../css/listagem_curso.css" />
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-
 
 <h2>Listagem de Cursos</h2>
 
 <div class="filtro">
-
-    <input type="text" id="nome" placeholder="Pesquisar" required autocomplete="off">
-    <a href="./cadastro.php">Novo</a>
+<a href="./cadastro.php">Novo</a>
+    <form method="get" action="">
+        <input type="text" name="filtro" placeholder="Pesquisar" value="<?= htmlspecialchars($filtro) ?>" required autocomplete="off">
+        <input type="submit" value="Pesquisar">
+    </form>
+   
     <div id="curso-lista"></div>
-    <script>
-        $(document).ready(function() {
-            
-            $("#nome").on("input", function() {
-                var query = $(this).val();
-                if (query.length > 1) {
-                    $.ajax({
-                        url: "buscar_cursos.php",
-                        method: "POST",
-                        data: { consulta: query },
-                        success: function(data) {
-                            $("#curso-lista").html(data);
-                        }
-                    });
-                } else {
-                    $("#curso-lista").html("");
-                }
-            });
-        });
-    </script>
-
-   
-   
 </div>
 
 <table class="listagem">
@@ -49,32 +35,30 @@ $resultado = $con->query("select * from cursos order by nome");
             <th>Opções</th>
         </tr>
     </thead>
-
     <tbody>
         <?php
-            while ($linha = $resultado->fetch_object()){
+        while ($linha = $resultado->fetch_object()) {
         ?>
-                <tr>
-                    <td><?=$linha->nome?></td>
-                    <td>
-                        <a href="excluir.php?id=<?=$linha->id?>">
-                            <img src="https://cdn-icons-png.flaticon.com/512/18/18297.png" alt="deletar" width="20" height="20">
-                        </a>
-
-                        <a href="editar.php?id=<?=$linha->id?>">
-                            <img src="https://cdn.icon-icons.com/icons2/3794/PNG/512/edit_drawing_draw_writing_write_pen_tool_icon_232789.png" alt="editar" width="20" height="20">
-                        </a>                        
-                    </td>
-                </tr>
+            <tr>
+                <td><?= htmlspecialchars($linha->nome) ?></td>
+                <td>
+                    <a href="excluir.php?id=<?= $linha->id ?>">
+                        <img src="https://cdn-icons-png.flaticon.com/512/18/18297.png" alt="deletar" width="20" height="20">
+                    </a>
+                    <a href="editar.php?id=<?= $linha->id ?>">
+                        <img src="https://cdn.icon-icons.com/icons2/3794/PNG/512/edit_drawing_draw_writing_write_pen_tool_icon_232789.png" alt="editar" width="20" height="20">
+                    </a>
+                </td>
+            </tr>
         <?php
-            }
+        }
         ?>
     </tbody>
 </table>
 
 <?php
-    $con->close();
+$stmt->close();
+$con->close();
 ?>
 
 <?php include("../layout_inferior.php") ?>
-
